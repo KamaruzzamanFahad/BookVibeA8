38d43fac71b035a08121920aafc3370339209c3e
KamaruzzamanFahad <mihacker41@gmail.com>
2024-03-27 04:12:33 +0600

    rror 404 page design done

--- src/Component/Error-page.jsx
+++ src/Component/Error-page.jsx
@@ -1,10 +1,17 @@
+import { useNavigate } from "react-router-dom";
 
 
 const ErrorPage = () => {
+    const navigate = useNavigate();
+    const clickhandle = () => {
+        navigate("/")
+    }
     return (
-        <div>
-            oops error 404 
-            <button>Back to home</button>
+        <div className="flex flex-col justify-center items-center">
+            <img src="https://img.freepik.com/free-vector/oops-404-error-with-broken-robot-concept-illustration_114360-5529.jpg" alt="" />
+            <h1 className="text-5xl font-bold text-center">404 - PAGE NOT FOUND</h1>
+            <p className="mt-4 text-center">The page you are looking for might have been removed<br></br> had its name changed or is temporarily unavailable.</p>
+            <button onClick={clickhandle} className="rounded-full bg-[#1f11b8] text-white px-7 mt-4">GO TO HOMEPAGE</button>
         </div>
     );
 };

3f740ada19b760ec9921a1523ff571faa44c68c6
KamaruzzamanFahad <mihacker41@gmail.com>
2024-03-28 23:42:34 +0600

    some bug fix

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
             <div className="navbar   flex justify-between">
                 <div className="navbar-start w-auto">
                     <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden text-black">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content 
@@ -38,7 +38,7 @@ const Header = () => {
                 </div>
                 <div className="w-auto navbar-end flex gap-5 justify-end">
                     <button className="buttom bg-[#23BE0A] text-white ">Sign In</button>
-                    <button className="buttom hidden sm:inline bg-[#59C6D2] text-white px-7">Sign Up</button>
+                    <button className="buttom bg-[#59C6D2] text-white px-7">Sign Up</button>
                     
                 </div>
             </div>
